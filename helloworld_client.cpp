4cf22feb0db3954a2ba473425431a86f08f94804
Bjarni Heselmans <12302152@student.pxl.be>
2025-03-25 14:33:53 +0100

    changed to dobbelsteen instead of subscriber counting

---
+++ helloworld_client.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <iostream>
+#include <zmq.hpp>
+
+int main(int argc, char* argv[]) {
+    // Controleer argumenten
+    if (argc < 3) {
+        std::cerr << "Gebruik: helloworld_client.exe <topic> <gebruikersnaam>\n";
+        return 1;
+    }
+
+    std::string topic = argv[1];
+    std::string naam = argv[2];
+
+    // Initialiseer ZMQ context
+    zmq::context_t context{1};
+
+    // PUSH socket om aanvragen te versturen
+    zmq::socket_t push_socket{context, zmq::socket_type::push};
+    push_socket.connect("tcp://benternet.pxl-ea-ict.be:24041");
+
+    // SUB socket om antwoorden te ontvangen
+    zmq::socket_t sub_socket{context, zmq::socket_type::sub};
+    sub_socket.connect("tcp://benternet.pxl-ea-ict.be:24042");
+
+    // Subscribe op antwoorden specifiek voor deze gebruiker en topic
+    std::string sub_topic = topic + "!>" + naam + ">";
+    sub_socket.setsockopt(ZMQ_SUBSCRIBE, sub_topic.c_str(), sub_topic.length());
+
+    // Verzend aanvraag via PUSH
+    std::string push_message = topic + "?>" + naam + ">";
+    std::cout << "Verzonden aanvraag: " << push_message << std::endl;
+    push_socket.send(zmq::buffer(push_message), zmq::send_flags::none);
+
+    // Luister naar antwoorden
+    zmq::message_t reply;
+    sub_socket.recv(reply, zmq::recv_flags::none);
+    std::string antwoord = reply.to_string();
+
+    std::cout << "Ontvangen antwoord: " << antwoord << std::endl;
+
+    // Houd venster open zodat antwoord leesbaar blijft
+    std::cout << "\nDruk ENTER om te sluiten...";
+    std::cin.get();
+
+    return 0;
+}
---
+++ helloworld_server.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <iostream>
+#include <random>
+#include <zmq.hpp>
+
+int main() {
+    zmq::context_t context(1);
+
+    // SUB socket luistert naar requests
+    zmq::socket_t sub_socket(context, zmq::socket_type::sub);
+    sub_socket.connect("tcp://benternet.pxl-ea-ict.be:24042");
+
+    // PUSH socket stuurt antwoorden
+    zmq::socket_t push_socket(context, zmq::socket_type::push);
+    push_socket.connect("tcp://benternet.pxl-ea-ict.be:24041");
+
+    // Subscribe op alle dobbelsteen aanvragen
+    std::string sub_topic = "dobbelsteen?>";
+    sub_socket.setsockopt(ZMQ_SUBSCRIBE, sub_topic.c_str(), sub_topic.length());
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(1, 6);
+
+    std::cout << "Dobbelsteen service actief op Benternet..." << std::endl;
+
+    while (true) {
+        zmq::message_t request;
+        sub_socket.recv(request, zmq::recv_flags::none);
+
+        std::string received_msg = request.to_string();
+        std::cout << "Ontvangen verzoek: " << received_msg << std::endl;
+
+        // Haal username uit bericht
+        std::string user = received_msg.substr(sub_topic.length());
+
+        // Doe een dobbelsteenworp
+        int roll = distrib(gen);
+
+        // Bouw het antwoordbericht volgens generieke structuur
+        std::string response = "dobbelsteen!>" + user + ">" + std::to_string(roll);
+
+        // Stuur het antwoord terug via PUSH
+        zmq::message_t msg(response.size());
+        memcpy(msg.data(), response.c_str(), response.size());
+
+        push_socket.send(msg, zmq::send_flags::none);
+        std::cout << "Antwoord verstuurd: " << response << std::endl;
+    }
+
+    return 0;
+}
